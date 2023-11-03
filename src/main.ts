d9818c85055073f1244e75977939d80b624bdb5c
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2023-11-03 21:14:20 +0300

    Добавлено отслеживание изменений нечистых объектов

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,41 @@
-import { applyPatches, getPatches } from "./utils";
+import { getPatches } from "./utils";
+
+class SomeStruct {
+  map = new Map();
+
+  set(key: string, value: string) {
+    this.map.set(key, value);
+  }
+}
 
 const data = {
   users: [
     {
       name: "John",
       age: 20,
+      birthday: new Date(),
+      tags: new Map(),
+      struct: new SomeStruct(),
     },
     {
       name: "Jane",
       age: 21,
+      birthday: new Date(),
+      tags: new Map(),
+      struct: new SomeStruct(),
     },
   ],
   usersCount: 2,
 };
 
 const patches = getPatches(data, (data) => {
-  data.users.push({ name: "Bob", age: 22 });
+  // console.log(data.users[0].struct.set);
+  data.users.push({ name: "Bob", age: 22, birthday: new Date(), tags: new Map(), struct: new SomeStruct() });
   data.usersCount = 3;
-  data.users.shift();
-  data.usersCount = 2;
+  data.users[0].birthday.setMinutes(20);
+  data.users[0].tags.set("foo", "bar");
+  data.users[0].struct.map.set("foo", "bar");
+  data.users[0].struct.set("foo2", "bar");
 });
 
 console.log(patches, data);
-
-const patchedData = applyPatches(data, patches);
-
-console.log(patchedData, data);
--- src/utils/get-patches.ts
+++ src/utils/get-patches.ts
@@ -1,12 +1,33 @@
 import { createDeepProxy } from "@novakod/deep-proxy";
 import { Patch, PatchType } from "../types";
+import { isPureObject } from "@novakod/is-pure-object";
 
 type MutateCb<Data extends object> = (data: Data) => void;
 
 export function getPatches<Data extends object>(data: Data, mutateCb: MutateCb<Data>): Patch[] {
   const patches: Patch[] = [];
 
-  const deepProxy = createDeepProxy(structuredClone(data), {
+  const deepProxy = createDeepProxy(data, {
+    get({ target, key, path, reciever }) {
+      if (typeof target[key] === "function" && !Array.isArray(target) && !isPureObject(target)) {
+        console.log("proxified function get: ", key, target);
+        return (...args: any[]) => {
+          const prevTarget = structuredClone(target);
+          const result = Reflect.apply(target[key], target, args);
+          const nextTarget = structuredClone(target);
+
+          patches.push({
+            type: "update",
+            path: path.slice(0, -1),
+            previousValue: prevTarget,
+            nextValue: nextTarget,
+          });
+          return result;
+        };
+      }
+
+      return Reflect.get(target, key, reciever);
+    },
     set({ target, key, value, path, reciever }) {
       let type: PatchType = "add";
       if (Object.hasOwn(target, key)) type = "update";
